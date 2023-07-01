f5d3039f9bf13ab3de8101a87bbc3165df0d94e2
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-07-01 09:17:26 -0600

    :construction: Add methods in purchase services
    return information on purchases according to the parameters sent

--- Server/src/main/java/com/group6/server/services/PurchaseService.java
+++ Server/src/main/java/com/group6/server/services/PurchaseService.java
@@ -3,6 +3,7 @@ package com.group6.server.services;
 import com.group6.server.models.entites.Event;
 import com.group6.server.models.entites.Purchase;
 import com.group6.server.models.entites.User;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -16,4 +17,8 @@ public interface PurchaseService {
 
     // This will be optional if needed in the future
     List<Purchase> findOneByEvent(Event event);
+
+    Page<Purchase> findAllByUser(User user, int page, int size);
+
+    List<Purchase> findAllById(String id);
 }
