fe5c62ce0e8100dc627e3f84f1a4f54307131f5b
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-07-01 09:13:27 -0600

    :construction: Add method in purchase PurchaseRepository
    The added method is used to return the purchases of a user and paginated

--- Server/src/main/java/com/group6/server/repositories/PurchaseRepository.java
+++ Server/src/main/java/com/group6/server/repositories/PurchaseRepository.java
@@ -1,7 +1,15 @@
 package com.group6.server.repositories;
 
 import com.group6.server.models.entites.Purchase;
+import com.group6.server.models.entites.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
+
+    //Encontrar todas las compras de un usuario
+    Page<Purchase> findAllByUser(User user, Pageable pageable);
+
+
 }
